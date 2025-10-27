abb89ab0c476182160cfe9e6ac9e24a483381f30
Bayajit islam <hire.bayajitislam@gmail.com>
2025-10-27 11:15:39 +0600

    Working On Food Screen

--- lib/features/home/screens/food_screen.dart
+++ lib/features/home/screens/food_screen.dart
@@ -1,10 +1,192 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:template/core/constants/app_colors.dart';
+import 'package:template/core/themes/app_text_style.dart';
 
 class FoodScreen extends StatelessWidget {
   const FoodScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      backgroundColor: const Color(0XFFF3D3AD),
+      body: Stack(
+        clipBehavior: Clip.none,
+        fit: StackFit.passthrough,
+        children: [
+          SizedBox(
+            height: 351.h,
+            child: Image.asset("assets/images/demoOnly.png"),
+          ),
+          Positioned(
+            top: 299.h,
+            left: 0,
+            right: 0,
+            child: Container(
+              decoration: BoxDecoration(
+                color: const Color(0XFFF3D3AD),
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(50.r),
+                  topRight: Radius.circular(50.r),
+                ),
+              ),
+              padding: EdgeInsets.symmetric(horizontal: 23.w, vertical: 39.h),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          //
+                          Container(
+                            decoration: BoxDecoration(
+                              color: AppColors.yellowishLightOrange,
+                              borderRadius: BorderRadius.circular(10.r),
+                              boxShadow: [
+                                BoxShadow(
+                                  color: Colors.black.withOpacity(0.06),
+                                  blurRadius: 10,
+                                  offset: Offset(0, 2),
+                                ),
+                              ],
+                            ),
+                            height: 100.h,
+                            width: 178.w,
+                            padding: EdgeInsets.all(16),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Calories",
+                                  style: AppTextStyles.s20w5P(
+                                    color: AppColors.icon,
+                                  ),
+                                ),
+                                Text(
+                                  "2,230 kcal",
+                                  style: AppTextStyles.s16w5P(
+                                    fontSize: 18,
+                                    color: AppColors.brandText,
+                                    fontweight: FontWeight.w400,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+
+                          //
+                        ],
+                      ),
+
+                      //
+                      _buildProgressCard(
+                        title: 'Protein',
+                        currentValue: 83,
+                        totalValue: 100,
+                      ),
+                    ],
+                  ),
+                  SizedBox(height: 13.h),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      _buildProgressCard(
+                        title: 'Carbs',
+                        currentValue: 83,
+                        totalValue: 100,
+                      ),
+
+                      _buildProgressCard(
+                        title: 'Fat',
+                        currentValue: 83,
+                        totalValue: 100,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
+}
+
+// Progress Card Method
+Widget _buildProgressCard({
+  required String title,
+  required int currentValue,
+  required int totalValue,
+  Color progressColor = AppColors.brandBorder,
+  Color backgroundColor = const Color(0xFFE8D5C4),
+}) {
+  double percentage = (currentValue / totalValue) * 100;
+
+  return Container(
+    decoration: BoxDecoration(
+      color: AppColors.yellowishLightOrange,
+      borderRadius: BorderRadius.circular(10.r),
+      boxShadow: [
+        BoxShadow(
+          color: Colors.black.withOpacity(0.06),
+          blurRadius: 10,
+          offset: Offset(0, 2),
+        ),
+      ],
+    ),
+    height: 100.h,
+    width: 178.w,
+    padding: EdgeInsets.all(16),
+    child: Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        // Title
+        Text(title, style: AppTextStyles.s20w5P(color: AppColors.icon)),
+        SizedBox(height: 12.h),
+
+        // Progress Bar
+        Stack(
+          children: [
+            // Background Bar
+            Container(
+              height: 6.h,
+              decoration: BoxDecoration(
+                color: backgroundColor,
+                borderRadius: BorderRadius.circular(4.r),
+              ),
+            ),
+            // Progress Bar
+            FractionallySizedBox(
+              widthFactor: currentValue / totalValue,
+              child: Container(
+                height: 6.h,
+                decoration: BoxDecoration(
+                  color: progressColor,
+                  borderRadius: BorderRadius.circular(4.r),
+                ),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 10.h),
+
+        // Percentage Values
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              '${percentage.toStringAsFixed(0)}%',
+              style: AppTextStyles.s14w4I(color: AppColors.icon),
+            ),
+            Text('100%', style: AppTextStyles.s14w4I(color: AppColors.icon)),
+          ],
+        ),
+      ],
+    ),
+  );
 }
